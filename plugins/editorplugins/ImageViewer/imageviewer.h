946ea7741b09acb4ebe1f84c2331421eaac703c3
arch <arch@MacBook-arch.local>
2010-04-02 13:58:20 +0400

    Trying to rename imageviewer folder

---
+++ plugins/editorplugins/ImageViewer/imageviewer.h
@@ -0,0 +1,53 @@
+#ifndef QMPQIMAGEVIEWER_H
+#define QMPQIMAGEVIEWER_H
+
+#include <QWidget>
+#include <QSettings>
+
+namespace Ui
+{
+    class ImageViewer;
+    class ImageSettingsDialog;
+}
+class ImageSettingsDialog;
+class QLabel;
+
+class ImageViewer : public QWidget
+{
+    Q_OBJECT
+    Q_PROPERTY(QImage image READ image WRITE setImage);
+    Q_PROPERTY(bool modified READ modified WRITE setModified);
+
+public:
+    ImageViewer(QWidget *parent = 0);
+    ~ImageViewer();
+    QImage image() { return m_image; }
+    bool modified() { return m_modified; }
+
+public slots:
+    void setImage(const QImage & image);
+//    void preferences();
+    void clear();
+    void copy();
+    void paste();
+    void zoomIn();
+    void zoomOut();
+    void zoomReset();
+    void setModified(bool modified);
+
+signals:
+    void modificationChanged(bool changed);
+
+private:
+    Ui::ImageViewer *ui;
+    QImage m_image;
+    QSettings * m_settings;
+    QLabel * label;
+    double scale;
+    bool m_modified;
+
+    void savePreferences(const ImageSettingsDialog * dialog);
+    void loadPreferences(ImageSettingsDialog * dialog);
+};
+
+#endif // QMPQIMAGEVIEWER_H
