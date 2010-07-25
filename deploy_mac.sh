54b0b273183abe9d9128e4e6579231aa8780be68
ABBAPOH <ABBAPOH@nextmail.ru>
2010-07-25 15:10:05 +0400

    Updated deploy script on mac Version changed to beta

--- deploy_mac.sh
+++ deploy_mac.sh
@@ -23,13 +23,17 @@ rm -R QMPQ.app/Contents/Frameworks/QtNetwork.framework/
 rm -R QMPQ.app/Contents/Frameworks/QtOpenGL.framework/
 rm QMPQ.app/Contents/MacOS/libCore.*
 rm -R QMPQ.app/Contents/MacOS/QMPQFileEngine.framework/
+
+echo "deploying translations"
+cp qmpq_ru.qm QMPQ.app/Contents/MacOS
+
+echo "deploying imageformats"
 mv QMPQ.app/Contents/MacOS/qmpqplugins/ QMPQ.app/Contents/PlugIns
 mv QMPQ.app/Contents/MacOS/imageformats/libQblpImagePlugin.1.0.0.dylib QMPQ.app/Contents/PlugIns/imageformats/libQblpImagePlugin.dylib
 mv QMPQ.app/Contents/MacOS/imageformats/libQtgaImagePlugin.1.0.0.dylib QMPQ.app/Contents/PlugIns/imageformats/libQtgaImagePlugin.dylib
 mv QMPQ.app/Contents/MacOS/imageformats/libQddsImagePlugin.1.0.0.dylib QMPQ.app/Contents/PlugIns/imageformats/libQddsImagePlugin.dylib
 rm -R QMPQ.app/Contents/MacOS/imageformats
 
-echo "deploying imageformats"
 deploylib QMPQ.app/Contents/PlugIns/imageformats/libQblpImagePlugin.dylib
 deploylib QMPQ.app/Contents/PlugIns/imageformats/libQtgaImagePlugin.dylib 
 deploylib QMPQ.app/Contents/PlugIns/imageformats/libQddsImagePlugin.dylib 
